d360a9d5548659361931f4771a463ea676e5705b
hemantsachdeva <hksachdeva007@gmail.com>
2020-09-29 13:58:49 +0530

    ARRAY:find greatest element

--- greatest_array_element.cpp
+++ greatest_array_element.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int array_size, i, temp = 0, isGreatest = 0;
+  int array_size, i, j, temp = 0, isGreatest = 0;
   cout << "Enter number of elements you want to enter in array:";
   cin >> array_size;
   int array[array_size];
@@ -10,10 +10,15 @@ int main() {
     cin >> array[i];
   }
   for (i = 1; i <= array_size; i++) {
-    if (array[0] < array[i]) {
-      array[0] = array[i];
+    for (j = 1; j <= array_size - i; j++) {
+      if (array[j] < array[j + 1]) {
+        temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
     }
+    isGreatest = array[1];
   }
-  cout << "Greates element of your array is:" << array[0];
+  cout << "Greatest element of your array is:" << isGreatest;
   return 0;
 }
